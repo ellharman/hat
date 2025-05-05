2910f450fb4a96ed3ee4cd1e68627c4006771a7c
Ell Harman <78617707+ellharman@users.noreply.github.com>
2025-05-05 11:02:03 +0100

    Fix mobile offclick

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -26,79 +26,82 @@ const Nav: React.FC = () => {
         Menu
       </button>
       {menuOpen && (
-        <div
-          className="absolute top-16 right-6 bg-secondary text-contrast rounded-lg shadow-lg"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <ul className="flex flex-col space-y-2">
-            <li>
-              <a href="/" className="block px-4 py-2 hover:bg-secondary/80">
-                Home
-              </a>
-            </li>
-            <li className="relative group">
-              <button
-                suppressHydrationWarning
-                className="block px-4 py-2 hover:bg-secondary/80"
-              >
-                Offerings
-              </button>
-              <ul className="absolute mt-2 right-10 bg-secondary text-contrast rounded-lg shadow-lg hidden group-hover:block group-focus-within:block">
-                <li>
-                  <a
-                    href="/offerings/introduction"
-                    className="block px-4 py-2 hover:bg-secondary/80"
-                  >
-                    Introduction
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/offerings/readings"
-                    className="block px-4 py-2 hover:bg-secondary/80"
-                  >
-                    Readings
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a
-                href="/about"
-                className="block px-4 py-2 hover:bg-secondary/80"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="/writing"
-                className="block px-4 py-2 hover:bg-secondary/80"
-              >
-                Writing
-              </a>
-            </li>
-            <li>
-              <a href="/shop" className="block px-4 py-2 hover:bg-secondary/80">
-                Shop
-              </a>
-            </li>
-            <li>
-              <a
-                href="/booking"
-                className="block px-4 py-2 hover:bg-secondary/80"
-              >
-                Booking
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
-      {menuOpen && (
-        <div
-          className="fixed inset-0 z-40 pointer-events-none"
-          onClick={() => setMenuOpen(false)}
-        ></div>
+        <>
+          <div
+            className="absolute top-16 right-6 bg-secondary text-contrast rounded-lg shadow-lg z-50"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <ul className="flex flex-col space-y-2">
+              <li>
+                <a href="/" className="block px-4 py-2 hover:bg-secondary/80">
+                  Home
+                </a>
+              </li>
+              <li className="relative group">
+                <button
+                  suppressHydrationWarning
+                  className="block px-4 py-2 hover:bg-secondary/80"
+                >
+                  Offerings
+                </button>
+                <ul className="absolute mt-2 right-10 bg-secondary text-contrast rounded-lg shadow-lg hidden group-hover:block group-focus-within:block">
+                  <li>
+                    <a
+                      href="/offerings/introduction"
+                      className="block px-4 py-2 hover:bg-secondary/80"
+                    >
+                      Introduction
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/offerings/readings"
+                      className="block px-4 py-2 hover:bg-secondary/80"
+                    >
+                      Readings
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a
+                  href="/about"
+                  className="block px-4 py-2 hover:bg-secondary/80"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/writing"
+                  className="block px-4 py-2 hover:bg-secondary/80"
+                >
+                  Writing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/shop"
+                  className="block px-4 py-2 hover:bg-secondary/80"
+                >
+                  Shop
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/booking"
+                  className="block px-4 py-2 hover:bg-secondary/80"
+                >
+                  Booking
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div
+            className="fixed inset-0 z-40"
+            onClick={() => setMenuOpen(false)}
+          ></div>
+        </>
       )}
     </nav>
   ) : (
