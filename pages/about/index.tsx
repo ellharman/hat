3d1bf1a9a8492c278ecd4e493841bb9e58c36dfc
Ell Harman <78617707+ellharman@users.noreply.github.com>
2025-07-08 14:59:28 +0100

    feat: about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,45 @@
+import { GetStaticProps } from "next";
+import fetchSB, { StoryVersion } from "../api/storyblok-fetch";
+import { PageStoryblok } from "../../component-types-sb";
+import Page from "../../components/Page";
+import InquiryForm from "../../components/InquiryForm";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetchSB("/stories", StoryVersion.DRAFT, `about`);
+  const { stories = [] } = await res.json();
+
+  const mostRecentStory = stories.reduce(
+    (
+      latest: {
+        created_at: string;
+      },
+      story: {
+        created_at: string;
+      }
+    ) => {
+      return new Date(story.created_at) > new Date(latest.created_at)
+        ? story
+        : latest;
+    },
+    stories[0]
+  );
+
+  return {
+    props: {
+      story: mostRecentStory || null,
+    },
+    revalidate: 30, // Revalidate every minute
+  };
+};
+
+const About: React.FC<PageStoryblok> = ({ story }) => {
+  return (
+    <>
+      {" "}
+      {story ? <Page story={story} /> : <div>Loading...</div>}
+      <InquiryForm />
+    </>
+  );
+};
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     e.preventDefault();
     setDrift(true);
     setTimeout(() => {
-      window.location.href = "/writing";
+      window.location.href = "/about";
     }, 300);
   };
   return (
@@ -28,7 +28,7 @@ const Home: React.FC = () => {
           className="hidden md:block mx-auto rounded-xl object-contain h-[66vh]"
         />
         <a
-          href="/writing"
+          href="/about"
           onClick={handleClick}
           className={`absolute left-1/2 top-1/2 z-20 px-16 py-8 bg-primary text-contrast text-2xl font-bold rounded-3xl shadow-lg transition-all duration-300 focus:outline-none focus:ring-4 focus:ring-primary-300 ${drift ? "translate-y-[-200%] opacity-0 pointer-events-none" : ""} -translate-x-1/2 -translate-y-1/2`}
           style={{
