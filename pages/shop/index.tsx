424055028b251c9b9a33362770066b08163eba00
Ell Harman <78617707+ellharman@users.noreply.github.com>
2025-08-08 14:55:17 +0100

    shop construct

---
+++ pages/shop/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Shop() {
+  return (
+    <div className="h-96 flex flex-col items-center justify-center text-center">
+      <h1 className="text-4xl">Under Construction</h1>
+      <p className="text-lg">
+        Check back soon...
+      </p>
+    </div>
+  );
+}
